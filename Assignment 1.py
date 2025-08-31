50d96a2c41c74dcbad872cf7f311517469cf87ce
ashokpatelap0987 <72006406+ashokpatelap0987@users.noreply.github.com>
2025-08-31 10:31:16 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,23 @@
+# Task 1
+'''
+a=int(input('Enter the first number:'))
+b=int(input('Enter the second number:'))
+add=a+b
+s=a-b
+m=a*b
+d=a/b
+
+print('Addition:',add)
+print('Subtraction:',s)
+print('Multiplication:',m)
+print('Division:',d)
+
+
+
+#Task 2
+
+fn=str(input('Enter your first name:'))
+ln=str(input('Enter your last name:'))
+print("Hello,",fn,ln,"! Welcome to the Python program")
+
+'''
